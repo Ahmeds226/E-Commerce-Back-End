20eda1b6cc66c4e1d0a2c2585de8d20c7d599c85
Ahmed Shahnawaz <iahmed.s1@icloud.com>
2022-07-28 19:42:29 +0100

    added validation to stock in product.js

--- models/Product.js
+++ models/Product.js
@@ -9,7 +9,6 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
     id: {
       type: DataTypes.INTEGER,
       allowNull: false,
@@ -31,7 +30,9 @@ Product.init(
       type: DataTypes.INTEGER,
       allowNull: false,
       defaultValue: 10,
-      isNumeric: true,
+      Validate: {
+        isNumeric: true,
+      },
     },
     category_id: {
       type: DataTypes.INTEGER,
