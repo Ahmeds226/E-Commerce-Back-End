d1969a6781f003abe125766941ed8ce50dc1fc66
Ahmed Shahnawaz <iahmed.s1@icloud.com>
2022-07-29 01:50:34 +0100

    created an api route to delete

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,7 +4,6 @@ const { Product, Category, Tag, ProductTag } = require("../../models");
 // The `/api/products` endpoint
 
 // get all products
-
 router.get("/", (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
@@ -29,7 +28,6 @@ router.get("/", (req, res) => {
 });
 
 // get one product
-
 router.get("/:id", (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
@@ -63,7 +61,6 @@ router.get("/:id", (req, res) => {
 });
 
 // create new product
-
 router.post("/", (req, res) => {
   Product.create({
     product_name: req.body.product_name,
@@ -94,7 +91,6 @@ router.post("/", (req, res) => {
 });
 
 // update product
-
 router.put("/:id", (req, res) => {
   // update product data
   Product.update(req.body, {
@@ -109,7 +105,6 @@ router.put("/:id", (req, res) => {
     .then((productTags) => {
       // get list of current tag_ids
       const productTagIds = productTags.map(({ tag_id }) => tag_id);
-
       // create filtered list of new tag_ids
       const newProductTags = req.body.tagIds
         .filter((tag_id) => !productTagIds.includes(tag_id))
@@ -119,7 +114,6 @@ router.put("/:id", (req, res) => {
             tag_id,
           };
         });
-
       // figure out which ones to remove
       const productTagsToRemove = productTags
         .filter(({ tag_id }) => !req.body.tagIds.includes(tag_id))
@@ -140,6 +134,22 @@ router.put("/:id", (req, res) => {
 
 router.delete("/:id", (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((dbProductData) => {
+      if (!dbProductData) {
+        rs.status(404).json({ message: "No product found with this id" });
+        return;
+      }
+      res.json(dbProductData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
